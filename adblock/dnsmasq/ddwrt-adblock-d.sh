19e64925009c50398af0762b5532565545efb07f
Erik Conijn <63402314+egc112@users.noreply.github.com>
2024-02-18 08:22:11 +0100

    Update ddwrt-adblock-d.sh

--- adblock/dnsmasq/ddwrt-adblock-d.sh
+++ adblock/dnsmasq/ddwrt-adblock-d.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #DEBUG=; set -x # comment/uncomment to disable/enable debug mode
 
-# name: ddwrt-adblock.sh
-# version: 0.91, 15-feb-2024, by egc, based on eibgrads ddwrt-blacklist-domains-adblock
+# name: ddwrt-adblock-d.sh
+# version: 0.92, 18-feb-2024, by egc, based on eibgrads ddwrt-blacklist-domains-adblock
 # purpose: blacklist specific domains in dnsmasq (dns) for DNSMasq > version 2.86 using local=/my.blockeddomain/
 # script type: shell script
 # installation:
 # 1. enable jffs2 (administration->jffs2) **or** use usb with jffs directory
 # 2. enable syslogd (services->services->system log)
-# 3. copy ddwrt-adblock.sh from https://github.com/egc112/ddwrt/tree/main/adblock to /jffs
-#    either with: curl -o /jffs/ddwrt-adblock.sh https://raw.githubusercontent.com/egc112/ddwrt/main/adblock/ddwrt-adblock.sh
+# 3. copy ddwrt-adblock-d.sh from https://github.com/egc112/ddwrt/tree/main/adblock/dnsmasq to /jffs
+#    either with: curl -o /jffs/ddwrt-adblock.sh https://raw.githubusercontent.com/egc112/ddwrt/main/adblock/dnsmasq/ddwrt-adblock-d.sh
 #    or by clicking the download icon in the upper right corner of the script
-# 4. make executable: chmod +x /jffs/ddwrt-adblock.sh
+# 4. make executable: chmod +x /jffs/ddwrt-adblock-d.sh
 # 5. add to Administration  > Commands: 
-#      /jffs/ddwrt-adblock.sh & 
+#      /jffs/ddwrt-adblock-d.sh & 
 #      if placed on USB then "Save USB" ; if jffs2 is used then : "Save Startup"
 #      Depending on the speed of your router or use of VPN, you might need to precede the command with: sleep 30
 # 6. add the following to the "additional dnsmasq options" field on the
@@ -21,11 +21,11 @@
 #     conf-dir=/tmp,*.blck
 #     /tmp/ is the directory where the blocklists: *.blck are placed and can be checked
 # 7. modify options e.g. URL list, MYWHITELIST and MYBLACKLIST:
-#     vi /jffs/ddwrt-adblock.sh 
+#     vi /jffs/ddwrt-adblock-d.sh 
 #     or edit with WinSCP
 # 8. (optional) enable cron (administration->management) and add the
 #     following job (runs daily at 4 a.m.):
-#     0 4 * * * root /jffs/ddwrt-adblock.sh
+#     0 4 * * * root /jffs/ddwrt-adblock-d.sh
 # 9. reboot
 #10. (optional) Prevent LAN clients to use their own DNS by ticking/enabling Forced DNS Redirection and
 #    Forced DNS Redirection DoT on Basic Setup page
